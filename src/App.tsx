2b7e364e54b34b27d38cdf14ae337fd47b08074d
dkdam <dkdam@live.com>
2023-03-20 23:03:51 +1100

    adding Compononents

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar />
       <h1> {count} Hotdogs </h1>
       <br />
       <button onClick={() => setCount(count => count + 1)}>
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="/">
+                Scented Craft
+            </a>
+            <ul>
+                <li><a href="/about"></a></li>
+                <li><a href="/login"></a></li>
+            </ul>
+        </nav>
+    )
+}
