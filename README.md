939ec2e45491c0d5d1b60474f0a994a0e9e891b3
dkdam <dkdam@live.com>
2023-03-20 21:31:09 +1100

    updating Index and App

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CraftedScent
+
+## 🚧 Under Construction 🚧
+
+<br/>
+
+## About
+A E-commerce project I like to do and more information will be coming soon...
+
+## Teck Stacks
+- Reactjs
+- Typescript
+- React-dom
--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1> {count} Hotdogs </h1>
+      <br />
+      <button onClick={() => setCount(count => count + 1)}>
+        Hotdog Button
+      </button>
+      <br />
+      <br />
+      <button onClick={() => setCount(0)}>
+        Reset Hotdogs
+      </button>
     </div>
   )
 }
