fede3c7609d8c095aa0fb26da366686791961e08
dkdam <dkdam@live.com>
2023-03-21 23:50:22 +1100

    udpated navbar routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,20 @@ import Navbar from './Components/NavBar'
 import Home from './Components/Home'
 import About from './Components/About'
 import Login from './Components/Login'
+import { Route, Routes } from 'react-router-dom'
 import './styles.css'
 
 function App() {
-  
-  let component
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home/>
-      break
-    case "/about":
-      component = <About/>
-      break
-    case "/login":
-      component = <Login/>
-      break
-  }
 
   return (
     <>
     <Navbar />
     <div className="container">
-      {component}
+      <Routes>
+        <Route path ='/' element = {<Home/>} />
+        <Route path ='/about' element = {<About/>} />
+        <Route path ='/login' element = {<Login/>} />
+      </Routes>
     </div>
     </>
   )
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,18 +1,33 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <nav className="nav">
-            <a href="/" className="site-title">
+            <Link to="/" className="site-title">
                 Scented Craft
-            </a>
+            </Link>
             <ul>
-                <li className="active">
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/login">Login</a>
-                </li>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/login">Login</CustomLink>
             </ul>
         </nav>
+    )
+}
+
+interface CustomLinkProps {
+    to: string;
+    children: React.ReactNode;
+}
+
+const CustomLink: FC<CustomLinkProps> = ({ to, children, ...props }) => {
+    const path = window.location.pathname
+
+    return(
+        <li className= {path === to ? "active" : ""}>
+            <Link to = {to} {...props}>
+                {children}
+            </Link>
+        </li>
     )
 }
