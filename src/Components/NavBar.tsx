12ccf1336b65468f31f1e621a1f27c54d6c87512
dkdam <dkdam@live.com>
2023-03-21 22:24:07 +1100

    basic navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,12 +2,16 @@
 export default function Navbar() {
     return (
         <nav className="nav">
-            <a href="/">
+            <a href="/" className="site-title">
                 Scented Craft
             </a>
             <ul>
-                <li><a href="/about">About</a></li>
-                <li><a href="/login">Login</a></li>
+                <li className="active">
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
             </ul>
         </nav>
     )
