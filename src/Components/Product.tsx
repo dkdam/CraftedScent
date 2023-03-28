5903c0b57203bb56b8729cd822703c690e6a34c2
dkdam <dkdam@live.com>
2023-03-28 22:26:45 +1100

    building product page

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Product() {
+
+    const [items,setItems] = useState([])
+
+    const store = async() => {
+        axios.get('http://fakestoreapi.com/products').then(res => {
+                setItems(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+    useEffect(() => {
+        store()
+    },[])
+
     return (
         <div>
             <h1>Product</h1>
             <p>Coming soon..</p>
+            {items.map((props) => {
+                return(
+                    <div>
+                        <div className="content">
+                            <h5>{props.title}</h5>
+                            <p>{props.description}</p>
+                            <p>${props.price}</p>
+                        </div>
+                        <img src={props.image} alt="" />
+                    </div>
+                )
+            })}
         </div>
     
         )
