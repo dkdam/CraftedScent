dbdfa0f7de34aa3eb548f8a546cdbe18b35fe2b3
dkdam <dkdam@live.com>
2023-04-02 00:34:20 +1100

    updated about page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,8 +1,29 @@
+import './About.css'
+
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
-            <p>It's a store.</p>
+        <div className='aboutContainer'>
+            <div className="aboutContent">
+                <h1>About Our Crafted Scents</h1>
+                <p>
+                    Our crafted scents are carefully curated from the finest ingredients
+                    sourced from around the world. Each scent is uniquely designed to
+                    evoke different emotions and experiences, and our team of expert perfumers
+                    works tirelessly to create scents that are both captivating and long-lasting.
+                </p>
+                <p>
+                    Our commitment to quality extends beyond our scents. We believe that
+                    every step of the customer experience should be exceptional, from the
+                    moment you place an order to the day your package arrives at your door.
+                    That's why we use only the best shipping and handling methods to ensure
+                    that your order is delivered on time and in perfect condition.
+                </p>
+                <p>
+                    Thank you for choosing our crafted scents. We hope that they bring
+                    joy and delight to your life, and we look forward to serving you
+                    for years to come.
+                </p>
+            </div>
         </div>
     )
 }
